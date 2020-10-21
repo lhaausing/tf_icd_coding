b0890cd0cb876614b9585d817ac1efabf6ab5166
Lee Haau-Sing <xl3119@nyu.edu>
2020-10-21 13:01:50 -0400

    update train.s.

--- train_eval.py
+++ train_eval.py
@@ -78,7 +78,7 @@ def train(model_name, train_loader, val_loader, inv_w, tokenizer, device, ngram_
         device_ids = [_ for _ in range(n_gpu)]
         model = torch.nn.DataParallel(model, device_ids=device_ids)
     optimizer = optim.Adam([p for p in model.parameters() if p.requires_grad], lr=float(lr), eps=float(eps))
-    if inv_w:
+    if inv_w != None:
         criterion = torch.nn.BCEWithLogitsLoss(pos_weight=inv_w)
     else:
         criterion = torch.nn.BCEWithLogitsLoss()

cdf42f900f2ad40f8a6fef84e1b8809d0482eddd
Lee Haau-Sing <xl3119@nyu.edu>
2020-10-26 21:01:39 -0400

    update script for bigpurple.

--- train_local_bert_bigpurple.s
+++ train_local_bert_bigpurple.s
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#SBATCH --partition=gpu8_medium
+#SBATCH --partition=gpu4_dev
 #SBATCH --nodes=1
-#SBATCH --tasks-per-node=8
+#SBATCH --tasks-per-node=4
 #SBATCH --cpus-per-task=1
-#SBATCH --time=2-00:00:00
+#SBATCH --time=4:00:00
 #SBATCH --mem-per-cpu=32G
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:4
 
 model_name=bert-base-uncased
-batch_size=64
-n_gpu=8
-n_epochs=40
+batch_size=32
+n_gpu=4
+n_epochs=20
 checkpt_path=../local_${model_name}_bs${batch_size}_sepcls.pt
 
 module load cuda91/toolkit/9.1.85

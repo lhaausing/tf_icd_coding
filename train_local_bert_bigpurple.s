3a78c357a3a3d53f470286b5e1a3d4b7cc4a4ee5
Lee Haau-Sing <xl3119@nyu.edu>
2020-10-26 20:51:56 -0400

    update script for bigpurple.

---
+++ train_local_bert_bigpurple.s
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu8_medium
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=8
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2-00:00:00
+#SBATCH --mem-per-cpu=32G
+#SBATCH --gres=gpu:8
+
+overlay_ext3=/scratch/xl3119/tf_icd/overlay-10GB-400K.ext3
+model_name=bert_base
+batch_size=64
+n_gpu=8
+n_epochs=40
+checkpt_path=../local_${model_name}_bs${batch_size}_sepcls.pt
+
+module load cuda91/toolkit/9.1.85
+python3 main.py \
+  --data_dir ../data \
+  --model_name ../${model_name} \
+  --local_model \
+  --n_epochs ${n_epochs} \
+  --batch_size ${batch_size} \
+  --checkpt_path ${checkpt_path} "

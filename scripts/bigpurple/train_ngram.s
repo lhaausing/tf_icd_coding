499d9c250896d3ab0a59fe242ca61944762b3cba
Lee Haau-Sing <xl3119@nyu.edu>
2020-11-17 12:58:07 -0500

    add bigpurple environments to master

---
+++ scripts/bigpurple/train_ngram.s
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu8_medium
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=8
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2-00:00:00
+#SBATCH --mem-per-cpu=32G
+#SBATCH --gres=gpu:8
+#SBATCH --mail-type=END
+#SBATCH --mail-user=xl3119@nyu.edu
+
+model_name=bert-base-uncased
+batch_size=64
+ngram_size=28
+n_gpu=8
+n_epochs=60
+seed=28
+checkpt_path=../ngram_bs${batch_size}_seed${seed}
+
+module load anaconda3/gpu/5.2.0
+module load cuda/10.1.105
+module load gcc/8.1.0
+conda activate bento
+
+cd /gpfs/scratch/xl3119/tf_icd_coding
+
+python run.py \
+  --seed ${seed} \
+  --data_dir ../data \
+  --model_name ${model_name} \
+  --n_epochs ${n_epochs} \
+  --batch_size ${batch_size} \
+  --ngram_size ${ngram_size} \
+  --use_ngram \
+  --sep_cls \
+  --n_gpu ${n_gpu} \
+  --checkpt_path ${checkpt_path} \
+  --save_best_f \
+  --save_best_auc

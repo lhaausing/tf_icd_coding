24b6729a4bf834b126ad6d93679131319352df43
Lee Haau-Sing <xl3119@nyu.edu>
2020-11-17 14:27:45 -0500

    Do not show long tokenizer.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 import os
+import re
 import glob
 import pickle
 import logging
@@ -24,6 +25,23 @@ from data import *
 from utils import *
 from models import *
 
+def set_global_logging_level(level=logging.ERROR, prefices=[""]):
+    """
+    Override logging levels of different modules based on their name as a prefix.
+    It needs to be invoked after the modules have been loaded so that their loggers have been initialized.
+
+    Args:
+        - level: desired level. e.g. logging.INFO. Optional. Default is logging.ERROR
+        - prefices: list of one or more str prefices to match (e.g. ["transformers", "torch"]). Optional.
+          Default is `[""]` to match all active loggers.
+          The match is a case-sensitive `module_name.startswith(prefix)`
+    """
+    prefix_re = re.compile(fr'^(?:{ "|".join(prefices) })')
+    for name in logging.root.manager.loggerDict:
+        if re.match(prefix_re, name):
+            logging.getLogger(name).setLevel(level)
+
+set_global_logging_level(logging.ERROR, ["transformers", "nlp", "torch", "tensorflow", "tensorboard", "wandb"])
 logger = logging.getLogger(__name__)
 
 def eval(args, model, val_loader):

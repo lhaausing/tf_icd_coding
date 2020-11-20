7e3ac7a5acc132bc1e71735bec30ccc9f9f0598b
Lee Haau-Sing <xl3119@nyu.edu>
2020-11-20 14:17:17 -0500

    fix bug.

--- get_preds.py
+++ get_preds.py
@@ -136,7 +136,7 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
 
     #get the exact seeds
-    seeds = '_'.split(args.seeds)
+    seeds = '-'.split(args.seeds)
     #get dataloaders
     if args.use_ngram:
         val_dataset = load_cache(args)[1]
